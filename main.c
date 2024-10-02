119c281dd7069f8f2dbc811614699bf66105639c
MartinMontagner <40306986m@gmail.com>
2024-10-02 13:11:30 -0300

    menu

--- main.c
+++ main.c
@@ -45,7 +45,94 @@ int main()
 
     printf("Hello world!\n");
     system("pause");
+    do
+    {
+        //system("cls");
+        mostrarMenuInicial();
+        printf("\nSeleccione una opcion: ");
+        scanf("%d", &opcion);
+
+        switch(opcion)
+        {
+        case 1:
+            printf("\n--- Ingresando al sistema... ---\n");
+            /// Logica de ingreso al sistema (login)
+            mostrarSubmenuIngreso();
+            break;
+
+        case 2:
+            printf("\n--- Registrarse en el sistema ---\n");
+            /// Logica de registro de usuario
+            mostrarSubmenuRegistro();
+            break;
+
+        case 0:
+            printf("\n--- Saliendo del sistema ---\n");
+            break;
+
+        default:
+            printf("\nOpcion no valida. Por favor, intenta nuevamente.\n");
+            break;
+        }
+    }
+    while(opcion != 0);
     return 0;
+}
+
+
+void setColor(int color)
+{
+    HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+
+void escribirConRetraso(const char *texto, int delay)
+{
+    while (*texto)
+    {
+        putchar(*texto++); // Imprime un carácter
+        fflush(stdout);    // Asegura que el carácter se imprima inmediatamente
+        usleep(delay * 1000); // Retraso en milisegundos
+    }
+}
+
+void mostrarMenuInicial()///menu inicial
+{
+    setColor(1);
+    escribirConRetraso("\n====================================", 5);
+    escribirConRetraso("\n    Sistema de Gestion de Libros", 5);
+    escribirConRetraso("\n====================================", 5);
+    escribirConRetraso("\n   1. Ingresar", 5);
+    escribirConRetraso("\n   2. Registrarse", 5);
+    escribirConRetraso("\n   0. Salir", 5);
+    escribirConRetraso("\n====================================\n", 5);
+    setColor(7);
+}
+
+void mostrarSubmenuIngreso()
+{
+    setColor(10);
+    printf("\n====================================\n");
+    printf("         Submenu de Ingreso\n");
+    printf("====================================\n");
+    printf("1. Iniciar sesion\n");
+    printf("2. Recuperar contrasena\n");
+    printf("3. Volver al menu principal\n");
+    printf("====================================\n");
+    setColor(7);
+}
+
+void mostrarSubmenuRegistro()
+{
+    setColor(12);
+    printf("\n====================================\n");
+    printf("        Submenu de Registro\n");
+    printf("====================================\n");
+    printf("1. Completar registro\n");
+    printf("2. Ver terminos y condiciones\n");
+    printf("3. Volver al menu principal\n");
+    printf("====================================\n");
+    setColor(7);
 }
 void cargarLibrosArchivoRandom(char nombreArchivo[])
 {
